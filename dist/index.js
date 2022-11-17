f9144d79cfb848dc20668be7b0458a43207889e0
Лепейко Егор Александрович <egor.lepeiko@nordclan.com>
2022-11-17 20:49:08 +0300

    Updates

--- dist/index.js
+++ dist/index.js
@@ -6,5 +6,20 @@ function init() {
     height: 900,
     backgroundColor: 11534335
   });
+  const text = new PIXI.Text("Hello ЖУЛЯ!", {
+    fontFamily: "Arial",
+    fontSize: 55,
+    fill: 16715792,
+    align: "center"
+  });
+  app.stage.addChild(text);
   document.body.appendChild(app.view);
+  setInterval(function() {
+    let right = true;
+    if (right == true && text.x < 1500) {
+      text.x += 3;
+    } else {
+      right = false;
+    }
+  }, 10);
 }
